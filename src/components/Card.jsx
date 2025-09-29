9ba0e3ecf05e93400b0f0230be098764cc1b44d8
Kasi Viswanath Reddy Chilakala <ch.kasiviswanathreddy@gmail.com>
2025-09-29 01:02:51 +0000

    Replaced image in header, added search functionality using  filter, addedp productData

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const Card = ({description, alt_description, id, user, urls, likes}) => {
+const Card = (props) => {
+  const {description, alt_description, id, user, urls, likes} =props;
 
   const style = {
     backgroundImage: `url(${urls.small})`
